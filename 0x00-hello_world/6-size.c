785bce91974bd1bb0fcda6129afe5cee2313c81b
Yehiakarim <yehiakarim90@gmail.com>
2018-06-21 05:36:58 +0000

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(unsigned long));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
-	return (0);
+	return (0;)
 
 }
