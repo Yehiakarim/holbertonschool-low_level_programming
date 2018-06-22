3bc46318709252f3125945b9e4546266eb4d3fdf
Yehiakarim <yehiakarim90@gmail.com>
2018-06-22 07:09:38 +0000

    new correction

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - description
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+	{
+		putchar(le);
+	}
+	putchar('\n');
+	return (0);
+}
