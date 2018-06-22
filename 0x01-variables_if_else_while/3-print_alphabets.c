9d8e27637e69b3b2b17a2f1efec2b1425bf437cb
Yehiakarim <yehiakarim90@gmail.com>
2018-06-22 07:54:10 +0000

    n

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - description
+*
+* Return: 0
+*/
+
+int main(void)
+{	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+	{
+		putchar(le);
+	}
+	for (le = 'A'; le <= 'Z'; le++)
+{
+	putchar(le);
+}
+	putchar('\n');
+	return (0);
+}
