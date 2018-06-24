c96dd68dee5ea7c0f11f9efd249ab5c94b20492c
Yehiakarim <yehiakarim90@gmail.com>
2018-06-24 03:55:12 +0000

    new correction

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - description
+*
+ Return: 0
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '8'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x >= y)
+				continue;
+			putchar(x);
+			putchar(y);
+			if (x == '8' && y == '9')
+				break;
+			putchar(',');
+			putchar(' ');
+				}
+	}
+	putchar('\n');
+	return(0);
+}
