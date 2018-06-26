1d566a46aa8550aaa09b98088b66da8ea1688c81
Yehiakarim <yehiakarim90@gmail.com>
2018-06-26 18:11:32 +0000

    new coreection

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,2 @@
-#ifindef "holberton.h"
-#define "holberton.h"
-
-/*
- * Structs, enums and unions definitions
- * Typedefs
- *Function prototypes
- */
-#endif /* "hoberton.h" */
-
 void print_alphabet(void);
 int _putchar(char c);
