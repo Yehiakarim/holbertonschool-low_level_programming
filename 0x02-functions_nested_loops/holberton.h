0e9e80ba23a6dea92473c22ffba418cf5aa51172
Yehiakarim <yehiakarim90@gmail.com>
2018-06-26 18:10:22 +0000

    H

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifindef "holberton.h"
+#define "holberton.h"
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ *Function prototypes
+ */
+#endif /* "hoberton.h" */
+
+void print_alphabet(void);
+int _putchar(char c);
