3f401044abc6e424995f0604e241b461a5dee17d
Yehiakarim <yehiakarim90@gmail.com>
2018-06-26 23:57:23 +0000

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _islower - check for lowercase character
+ *@c: the number to be checked
+ * Return: 1 if c is lowercase && otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+
+			return (0);
+	}
+	return (0);
+}
