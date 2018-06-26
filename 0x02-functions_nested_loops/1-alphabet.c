d391d6871880c7dddf394581dc7d5d62562b4583
Yehiakarim <yehiakarim90@gmail.com>
2018-06-26 16:32:01 +0000

    print az

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *
+ * main - description
+ *
+ * REturn: 0
+ */
+void print_alphabet(void)
+{
+	int b;
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
