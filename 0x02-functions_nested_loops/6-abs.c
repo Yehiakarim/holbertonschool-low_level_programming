5720a0077a0ace52641f9c20e9c3703d1a7c8926
Yehiakarim <yehiakarim90@gmail.com>
2018-06-27 04:46:10 +0000

    correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,15 +9,15 @@ int _abs(int n)
 
 	if (n < 0)
 	{
-		return(n * -1);
+		return (n * -1);
 	}
 	else if (n > 0)
 	{
-		return(n);
+		return (n);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	return (0);
 }
