0eb818a3e00344ba1076fa8498da6668349f4540
Yehiakarim <yehiakarim90@gmail.com>
2018-06-27 05:31:49 +0000

    new file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 /**
- * _isalpha - checks for alphabetic character
- *@n: the number to be checked
- * Return: 1 if c is lowercase or uppercase && otherwise 0
+ * print_last_digit - prints the last digit of a number
+ *@n: prints the last digit of a number
+ * Return: 0
  */
 int print_last_digit(int n)
 {
 	if (n > 0)
 	{
-		return((n + 0) % 10);
+		return ((n + 0) % 10);
 	}
 	else if (n < 0)
 	{
-		return((n + 0) * -1 % 10);
+		return ((n + 0) * -1 % 10);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	return (0);
 }
