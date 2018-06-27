c478535a7ea34d629d29df2af0f917d07b259638
Yehiakarim <yehiakarim90@gmail.com>
2018-06-27 07:43:50 +0000

    m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print - add adds two integers and returns the result
+*@n: adds two integers and returns the result
+*@a: adds two integers and returns the result
+* Return: 0
+*/
+int add(int n, int a)
+{
+	n = 50;
+	a = 56;
+	return (n + '0');
+	return (a + '0');
+	_putchar(n);
+	_putchar(a);
+}
