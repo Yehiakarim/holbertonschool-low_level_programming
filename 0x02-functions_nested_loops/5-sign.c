03d66126254c1397f5f802890faeec06dba315c0
Yehiakarim <yehiakarim90@gmail.com>
2018-06-27 00:59:18 +0000

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to be checked
+ * Return: 1 if n is greater than zero && otherwise 0
+ */
+int print_sign(int n)
+{
+	if (n >= '1' && n <= '9')
+	{
+		_putchar('+');
+		return (1);
+	}
+	if ( n == '0')
+	{
+		_putchar('0');
+		return (0);
+	}
+	if ( n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+}
