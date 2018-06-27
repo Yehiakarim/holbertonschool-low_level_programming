268beaaa8187217de427d74df8aeec1075a70ba8
Yehiakarim <yehiakarim90@gmail.com>
2018-06-27 01:31:57 +0000

    correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,21 @@
  */
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
 		return (0);
 	}
-	if (n < 48)
-	{
-		_putchar('-');
-		return (-1);
-	}
 
-	return (0);
 }
