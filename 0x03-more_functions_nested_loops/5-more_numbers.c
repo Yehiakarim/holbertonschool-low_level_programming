74ac1d55ae9a7f50cb9967cebdb08220e90e0153
Yehiakarim <yehiakarim90@gmail.com>
2018-06-29 03:58:05 +0000

    m

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * more_numbers - print
+ *@c; print
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
