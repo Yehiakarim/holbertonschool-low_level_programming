a7abe859d32f84232811893f5b37187a523f3a54
Yehiakarim <yehiakarim90@gmail.com>
2018-06-29 04:45:55 +0000

    new correction

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,18 @@
-#include "holberton.h"
 
+#include "holberton.h"
+/**
+*print_diagonal - decription
+* @n: - is good
+* Return: 0
+*/
 void print_diagonal(int n)
 {
 	int x, z;
 	{
-		for ( x = 0; x < n; x++)
+		for (x = 0; x < n; x++)
 		{
-			for ( z = 0; z < n; z++)
-				if ( z < x)
+			for (z = 0; z < n; z++)
+				if (z < x)
 				{
 					_putchar(' ');
 				}
