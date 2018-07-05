e15cb3b19610d76ca24df12093d5f0056ec91824
Yehiakarim <yehiakarim90@gmail.com>
2018-07-05 04:18:53 +0000

    new

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcpy - function copies the string
+ * @dest - parametar
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
