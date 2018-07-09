2f42235e03588cdfce44dfff75e3d12ded99d6c9
Yehiakarim <yehiakarim90@gmail.com>
2018-07-09 04:15:56 +0000

    ew

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ **string_toupper - descriptio
+ *@p: pointer
+ *Return: s
+ */
+char *string_toupper(char *p)
+
+{
+	int i;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 32;
+		}
+	}
+	return (p);
+}
