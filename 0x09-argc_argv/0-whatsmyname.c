8d5b6b94c93604a3494c668cb4dd19ed6f1bdb42
Yehiakarim <yehiakarim90@gmail.com>
2018-07-14 18:16:53 +0000

    correction

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ * main - prints program name
+ * @argc: argument
+ * @argv: array
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
